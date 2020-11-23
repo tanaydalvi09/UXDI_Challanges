0236b4fecba8b9d860b64560db7abfceeff7a888
tanaydalvi09 <32214353+tanaydalvi09@users.noreply.github.com>
2020-11-23 17:47:16 +0100

    Delete challange 1.js

--- challange 1.js
+++
@@ -1,7 +0,0 @@
-function LifePathNumber(birthDate) {
-    return [...birthDate.split('-')].map(v=>v.split('').reduce((m,n)=>m+n*1,0))
-   .map(v=>v.toString().split('').reduce((m,n)=>m+n*1,0)).reduce((m,n)=>m+n*1,0).toString().split('')
-   .reduce((m,n)=>m+n*1,0).toString().split('').reduce((m,n)=>m+n*1,0)
-  }
-
-  console.log(LifePathNumber("1879-03-14"));
