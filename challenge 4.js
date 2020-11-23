67d985c24da7123dd1e37060c7aee684b38e4020
tanaydalvi09 <32214353+tanaydalvi09@users.noreply.github.com>
2020-11-23 15:40:42 +0100

    Challenge 4

---
+++ challenge 4.js
@@ -0,0 +1,10 @@
+function cal(s, d){
+    if (s < 0){
+            t = 0;
+            console.log(t);
+            } else {
+            t = d / s ;
+            console.log(t);
+         }
+}
+cal(5,400);
