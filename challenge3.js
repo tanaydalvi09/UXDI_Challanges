3a1d37c0544d447438c7b4364e99de20bdb9369e
tanaydalvi09 <32214353+tanaydalvi09@users.noreply.github.com>
2020-11-23 10:44:02 +0100

    Challange 3

---
+++ challenge3.js
@@ -0,0 +1,6 @@
+let name='TanaY'
+let big=name.split('').filter(n => n===n.toUpperCase()).length;
+console.log(big);
+let small=name.length-big;
+console.log(small);
+console.log((big>small)?name.toUpperCase():name.toLowerCase());
